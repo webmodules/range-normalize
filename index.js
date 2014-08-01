0633581d1b25f1e1e3ffc03a40886fcf741f9282
Nathan Rajlich <nathan@tootallnate.net>
2014-07-31 20:10:32 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+
+/**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('range-normalize');
+
+/**
+ * Module exports.
+ */
+
+module.exports = normalize;
+
+/**
+ * "Normalizes" the DOM Range instance, such that slight variations in the start
+ * and end containers end up being normalized to the same "base" representation.
+ * The aim is to always have `startContainer` and `endContainer` pointing to
+ * a TextNode instance.
+ *
+ * @param {Range} range - DOM Range instance to "normalize"
+ * @return {Range} returns `range`, after being "normalized"
+ */
+
+function normalize (range) {
+  var sc = range.startContainer;
+  var so = range.startOffset;
+  var ec = range.endContainer;
+  var eo = range.endOffset;
+
+
+  if (sc.nodeType === 3 && so === sc.nodeValue.length) {
+    debug('start is at end of TextNode, need to move to `nextSibling`');
+
+    while (sc && !sc.nextSibling) {
+      console.log(sc.nextSibling);
+      sc = sc.parentNode;
+    }
+
+    if (sc) {
+      sc = sc.nextSibling;
+      so = 0;
+    } else {
+      debug('could not find TextNode within %o, resetting `sc`', range.startContainer);
+      sc = range.startContainer;
+    }
+  }
+
+
+  if (sc.nodeType === 1) {
+    debug('start is an Element, need to find deepest `firstChild` TextNode');
+
+    while (sc && sc.nodeType !== 3) {
+      sc = sc.firstChild;
+    }
+    console.log(sc);
+
+    if (sc) {
+      so = 0;
+    } else {
+      debug('could not find TextNode within %o, resetting `sc`', range.startContainer);
+      sc = range.startContainer;
+    }
+  }
+
+
+
+  if (ec.nodeType === 3 && eo === 0) {
+    debug('end is at start of TextNode, need to move to `previousSibling`');
+
+    while (ec && !ec.previousSibling) {
+      console.log(ec.previousSibling);
+      ec = ec.parentNode;
+    }
+
+    ec = ec.previousSibling;
+  }
+
+
+  if (ec.nodeType === 1) {
+    debug('end is an Element, need to find deepest `lastChild` TextNode');
+
+    while (ec && ec.nodeType !== 3) {
+      ec = ec.lastChild;
+    }
+    console.log(ec);
+
+    if (ec) {
+      eo = ec.nodeValue.length;
+    } else {
+      debug('could not find TextNode within %o, resetting `ec`', range.endContainer);
+      ec = range.endContainer;
+    }
+  }
+
+
+
+  debug('normalizing to %o %o, %o %o:', sc, so, ec, eo);
+  range.setStart(sc, so);
+  range.setEnd(ec, eo);
+
+  return range;
+}
---
+++ test/test.js
@@ -0,0 +1,45 @@
+
+var assert = require('assert');
+var normalize = require('../');
+
+describe('range-normalize', function () {
+
+  it('should normalize a Range wrapping the outside of <b> node', function () {
+    var div = document.createElement('div');
+    div.innerHTML = '<i>asdf </i><b>asdf</b> asdf';
+
+    var range = document.createRange();
+    range.setStart(div.childNodes[0].firstChild, div.childNodes[0].firstChild.nodeValue.length);
+    range.setEnd(div.childNodes[2], 0);
+
+    // normalize Range
+    normalize(range);
+
+    // test that the Range is normalized to the inner TextNode of the <b>
+    console.log(range);
+    assert(range.startContainer === div.childNodes[1].firstChild, '`startContainer` doesn\'t match');
+    assert(range.startOffset === 0);
+    assert(range.endContainer === div.childNodes[1].firstChild);
+    assert(range.endOffset === 4);
+  });
+
+  it('should normalize a Range wrapping a <b> node', function () {
+    var div = document.createElement('div');
+    div.innerHTML = '<i>asdf </i><b>asdf</b> asdf';
+
+    var range = document.createRange();
+    range.setStart(div.childNodes[1], 0);
+    range.setEnd(div.childNodes[1], 1);
+
+    // normalize Range
+    normalize(range);
+
+    // test that the Range is normalized to the inner TextNode of the <b>
+    console.log(range);
+    assert(range.startContainer === div.childNodes[1].firstChild, '`startContainer` doesn\'t match');
+    assert(range.startOffset === 0);
+    assert(range.endContainer === div.childNodes[1].firstChild);
+    assert(range.endOffset === 4);
+  });
+
+});
