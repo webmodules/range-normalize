78a9e5a12742440edb97363d05b8496e255d528c
Nathan Rajlich <nathan@tootallnate.net>
2014-07-31 20:35:33 -0700

    test: add inner-wrapping TextNode test

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ describe('range-normalize', function () {
     div.innerHTML = '<i>asdf </i><b>asdf</b> asdf';
 
     var range = document.createRange();
-    range.setStart(div.childNodes[0].firstChild, div.childNodes[0].firstChild.nodeValue.length);
+    range.setStart(div.childNodes[0].firstChild, 5);
     range.setEnd(div.childNodes[2], 0);
 
     // normalize Range
@@ -30,6 +30,25 @@ describe('range-normalize', function () {
     var range = document.createRange();
     range.setStart(div.childNodes[1], 0);
     range.setEnd(div.childNodes[1], 1);
+
+    // normalize Range
+    normalize(range);
+
+    // test that the Range is normalized to the inner TextNode of the <b>
+    console.log(range);
+    assert(range.startContainer === div.childNodes[1].firstChild, '`startContainer` doesn\'t match');
+    assert(range.startOffset === 0);
+    assert(range.endContainer === div.childNodes[1].firstChild);
+    assert(range.endOffset === 4);
+  });
+
+  it('should leave as-is a Range wrapping text inside a <b> node', function () {
+    var div = document.createElement('div');
+    div.innerHTML = '<i>asdf </i><b>asdf</b> asdf';
+
+    var range = document.createRange();
+    range.setStart(div.childNodes[1].firstChild, 0);
+    range.setEnd(div.childNodes[1].firstChild, 4);
 
     // normalize Range
     normalize(range);
